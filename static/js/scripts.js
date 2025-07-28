49c462c656f20bad148c9d9946fd62d229c21a6e
QuantummyKnight <136607174+QuantummyKnight@users.noreply.github.com>
2025-07-28 17:10:06 +0800

    v9

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'patents']
+const section_names = ['home', 'projects' , 'publications', 'patents']
 
 
 window.addEventListener('DOMContentLoaded', event => {
